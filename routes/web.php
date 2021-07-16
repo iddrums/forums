aaa741681f9ff48bdd5729b88db71d14396a55c0
iddrums <48152012+iddrums@users.noreply.github.com>
2021-07-16 09:06:43 +0100

    episode 41

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,8 @@ Route::get('/threads/{channel}/{thread}/replies', [App\Http\Controllers\RepliesC
 Route::post('/threads/{channel}/{thread}/replies', [App\Http\Controllers\RepliesController::class, 'store'])->name('store');
 Route::patch('/replies/{reply}', [App\Http\Controllers\RepliesController::class, 'update']);
 Route::delete('/replies/{reply}', [App\Http\Controllers\RepliesController::class, 'destroy']);
+Route::post('/threads/{channel}/{thread}/subscriptions', [App\Http\Controllers\ThreadSubscriptionsController::class, 'store'])->middleware('auth');
+
 
 
 Route::post('/replies/{reply}/favorites', [App\Http\Controllers\FavoritesController::class, 'store']);
---
+++ tests/Feature/SubscribeToThreadsTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Thread;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class SubscribeToThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function a_user_can_subscribe_to_threads()
+    {
+        $this->signIn();
+
+        $thread = Thread::factory()->create();
+
+        $this->post($thread->path() . '/subscriptions');
+
+        $thread->addReply([
+           'user_id' => auth()->id(),
+           'body' => 'Some reply here'
+        ]);
+
+        // $this->assertCount(1, $thread->subscriptions);
+    }
+
+}
