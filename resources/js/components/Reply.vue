554848014c3b1880682cf7c7664051035ed587fb
iddrums <48152012+iddrums@users.noreply.github.com>
2021-07-26 13:04:16 +0100

    episode 83

--- resources/js/components/Reply.vue
+++ resources/js/components/Reply.vue
@@ -4,13 +4,13 @@
             <div class="card-header">
                 <div class="level">
                     <h5 class="flex">
-                    <a :href="'/profiles/'+data.owner.name"
-                        v-text="data.owner.name">
+                    <a :href="'/profiles/' + reply.owner.name"
+                        v-text="reply.owner.name">
                     </a> said <span v-text="ago"></span>
                     </h5>
 
                     <div v-if="signedIn">
-                        <favorite :reply="data"></favorite>
+                        <favorite :reply="reply"></favorite>
                     </div>
                </div>
             </div>
@@ -31,13 +31,13 @@
                 <div v-else v-html="body"></div>
             </div>
 
-              <div class="card-footer level">
-                  <div v-if="authorize('updateReply', reply)">
+              <div class="card-footer level" v-if="authorize('owns', reply) || authorize('owns', reply.thread)">
+                  <div v-if="authorize('owns', reply)">
                     <button class="btn btn-xs mr-1" @click="editing = true">Edit</button>
                     <button class="btn btn-xs btn-danger mr-1" @click="destroy">Delete</button>
 
                   </div>
-                    <button class="btn btn-xs btn-success ml-a" @click="markBestReply" v-show="! isBest">Best Reply?</button>
+                    <button class="btn btn-xs btn-success ml-a" @click="markBestReply" v-if="authorize('owns', reply.Thread)">Best Reply?</button>
               </div>
         </div>
       </div>
@@ -49,28 +49,22 @@ import Favorite from './Favorite.vue';
 import moment from 'moment';
 
     export default {
-        props: ['data'],
+        props: ['reply'],
 
         components: { Favorite },
 
         data() {
             return {
                 editing: false,
-                id: this.data.id,
-                body: this.data.body,
-                // isBest: this.data.isBest,
-                reply: this.data,
-                thread: window.thread
+                id: this.reply.id,
+                body: this.reply.body,
+                isBest: this.reply.isBest
             };
         },
 
         computed: {
-            isBest () {
-               return this.thread.best_reply_id == this.id;
-            },
-
             ago() {
-              return moment(this.data.created_at).fromNow() +'...';
+              return moment(this.reply.created_at).fromNow() +'...';
             }
 
         },
@@ -78,7 +72,7 @@ import moment from 'moment';
         methods: {
             update() {
                axios.patch(
-                    '/replies/' + this.data.id, {
+                    '/replies/' + this.id, {
                         body: this.body
                     })
                     .catch(error => {
@@ -91,16 +85,16 @@ import moment from 'moment';
             },
 
             destroy() {
-                 axios.delete('/replies/' + this.data.id);
+                 axios.delete('/replies/' + this.id);
 
-                  this.$emit('deleted', this.data.id);
+                  this.$emit('deleted', this.id);
 
             },
 
             markBestReply() {
               axios.post('/replies/' + this.id + '/best');
 
-              window.events.$emit('best-reply-selected', this.data.id);
+              window.events.$emit('best-reply-selected', this.id);
             }
         },
     }
